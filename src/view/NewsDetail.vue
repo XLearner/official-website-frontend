04ff33ac561bef4753f68855b90126abd5f03161
Yim_Li <yan95501@outlook.com>
2024-08-26 13:01:05 +0800

    feat: 添加图片占位图和加载动画，调整新闻详情界面导航菜单栏

--- src/view/NewsDetail.vue
+++ src/view/NewsDetail.vue
@@ -4,12 +4,15 @@
         <div class="container">
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-3" id="myScrollspy">
-                    <ul class="nav nav-tabs nav-stacked center-block" id="news">
-                        <p>新闻列表</p>
-                        <li :class="item.id == id ? 'active' : ''" v-for="(item, index) in newsNav" :key="index">
-                            <div @click="linkTo(item.id)">{{ item.title }}</div>
-                        </li>
-                    </ul>
+                    <div class="nav-box" >
+                        <div class="icon" @click="showList"><img src="../assets/img/list-text.png" alt=""></div>
+                        <ul class="nav nav-tabs nav-stacked center-block" :class="{'show': ifShowList}" id="news">
+                            <p>新闻列表</p>
+                            <li :class="item.id == id ? 'active' : ''" v-for="(item, index) in newsNav" :key="index">
+                                <div @click="linkTo(item.id)">{{ item.title }}</div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-9 content  wow zoomIn">
                     <div class="content-block" v-show="ifShow">
@@ -17,7 +20,8 @@
                             {{ newsItem.title }}
                             <small>/ {{ newsItem.time }}</small>
                         </h2>
-                        <img class="img" :src="newsItem.outImg" alt="示例图">
+                        <img v-show="ifLoaded" class="img" :src="newsItem.outImg" alt="示例图" @load="picReady">
+                        <div v-show="!ifLoaded" class="loading-img"><img src="../assets/img/img_oc.png" alt="占位图"></div>
                         <div class="txt" v-html="newsItem.content"></div>
                     </div>
                 </div>
@@ -35,6 +39,7 @@ export default {
         return {
             id: "section-1",
             ifShow: false,
+            ifShowList: false,
             newsList: [
             ],
             newsNav: [],
@@ -44,14 +49,15 @@ export default {
                 eng_tit: '',
                 content: '',
             },
+            ifLoaded: false
             // serviceContentList: []
         };
     },
     created() {
         apiGetNews().then(res => {
             if (res.code >= 0) {
-            const data = res.data.sort((a,b) => b.time - a.time)
-            this.$data.newsNav = data.map(item => ({
+                const data = res.data.sort((a, b) => b.time - a.time)
+                this.$data.newsNav = data.map(item => ({
                     id: item.id,
                     title: item.title
                 }));
@@ -78,19 +84,29 @@ export default {
     methods: {
         linkTo(id) {
             this.hideCtn();
-            this.newsItem = {};
+            this.$data.newsItem = {};
+
             this.$router.push({ path: '/news/' + id });
             this.$data.newsItem = this.newsList.filter(ele => {
                 return ele.id == id
             })[0]
             this.$data.id = id;
             this.showCtn();
+            this.ifShowList = false;
         },
         showCtn() {
             this.$data.ifShow = true;
         },
         hideCtn() {
             this.$data.ifShow = false;
+            this.$data.ifLoaded = false;
+        },
+        picReady() {
+            this.$data.ifLoaded = true;
+            console.log('ready')
+        },
+        showList() {
+            this.ifShowList = !this.ifShowList;
         }
     }
 };
@@ -167,6 +183,9 @@ ul.nav-tabs li:last-child div {
 ul.nav-tabs.affix {
     top: 30px;
 }
+.icon {
+    display: none;
+}
 
 .content-block {
     margin: 50px 0;
@@ -180,25 +199,88 @@ ul.nav-tabs.affix {
 .content-block img {
     max-width: 100%;
 }
+
 .content .img {
     margin-bottom: 10px;
 }
+
 .content .txt {
     font-size: 14px;
     line-height: 1.5;
 }
 
-@media screen and (max-width: 767px) {
+@media screen and (max-width: 992px) {
     #NewsDetail .container {
         padding-bottom: 20px;
         min-height: 500px;
+    }
+
+    .nav-box {
+        position: fixed;
+        z-index: 10;
+        top: 4.8rem;
+    }
+
+    .icon {
+        display: block;
+        position: absolute;
+        width: 3.5rem;
+        height: 3.5rem;
+        padding: 8px;
+        top: 4rem;
+        left: 0;
+        border-radius: 50%;
+        background: rgba(233, 233, 233, .3);
+    }
+    .icon img {
+        display: block;
+        width: 100%;
+    }
+
+    ul.nav-tabs {
+        height: 60rem;
+        overflow: auto;
+        display: none;
+    }
+    ul.nav-tabs.show {
+        display: block;
+    }
+}
+
+.loading-img {
+    position: relative;
+    overflow: hidden;
+}
+
+.loading-img::after {
+    content: "";
+    display: block;
+    width: 80px;
+    height: 1500px;
+    background-color: rgba(255, 255, 255, .7);
+    position: absolute;
+    box-shadow: 0 0 25px #fff;
+    left: 0;
+    top: 0;
+    animation-duration: 2s;
+    animation-name: loadImg;
+    animation-iteration-count: infinite;
+}
+
+@keyframes loadImg {
+    from {
+        transform: rotate(-45deg) translate(-120px, -210px);
+    }
+
+    to {
+        transform: rotate(-45deg) translate(1260px, 180px)
     }
 }
 </style>
 
 <style>
-    
-#NewsDetail .content-block ul, #NewsDetail .content-block ol{
+#NewsDetail .content-block ul,
+#NewsDetail .content-block ol {
     list-style: auto;
     margin-left: 20px;
 }
